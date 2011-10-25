db92058b9522a06bbb2c66232589bfb0004623fc
usuario <usuario@seila.(none)>
2011-10-25 14:23:25 -0200

    layout e autenticacao criptografada basica

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verso::Application.config.secret_token = '5370fb26a6bf8b1e3dca3c57a8e1e1210c594e9cfab30b2634aedc9ba834a6e7d8a8d78c066c4d45e567923318e2e20d0c0567685e2f6cf9f2a91180126dab95'
