330d8e7b07850946bd6f619bc3a48213bc6feebc
Thiago Almeida <tapgyn@gmail.com>
2011-11-09 14:13:47 -0200

    bug corrigido. SWF ficava por cima do popup

--- app/helpers/project_versions_helper.rb
+++ app/helpers/project_versions_helper.rb
@@ -18,7 +18,8 @@ classid='clsid:d27cdb6e-ae6d-11cf-96b8-444553540000'
 codebase='http://fpdownload.macromedia.com/
 pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0'>
 <param name='SRC' value='#{paperclip.url}'>
-<embed src='#{paperclip.url}' width='800' height='600'></embed>
+<param name='wmode' value='opaque'/>
+<embed src='#{paperclip.url}' wmode='opaque' width='800' height='600'></embed>
 </object>"
     link_to "#", :rel=>'swf', :'data-swf-content' => s, :Style=>'display:inline; float:left; width: 100px; text-align:center' do
       raw [
