759c5abf457904b2e5b5296023b57a82b2e5c875
Thiago Almeida <tapgyn@gmail.com>
2011-11-18 11:20:26 -0200

    Guard tinha uma duplicidade. Fabrication integrada aos geradores rails

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,11 @@ module Verso
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
 
+    config.generators do |g|
+      g.test_framework      :rspec, :fixture => true
+      g.fixture_replacement :fabrication
+    end
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
